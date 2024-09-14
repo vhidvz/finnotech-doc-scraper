45342a6913d5dc00ffddef7a7b2cd425c636bb29
Vahid V. <vhid.vz@gmail.com>
2024-09-14 12:20:37 +0330

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+from tqdm import tqdm
 from playwright.sync_api import sync_playwright
 
 
@@ -5,6 +7,8 @@ HOME_PAGE = "https://finnotech.ir/doc/"
 
 
 if __name__ == '__main__':
+    result = []
+
     # Start Playwright and open a browser
     with sync_playwright() as p:
         # You can set headless=False to see the browser window
@@ -13,6 +17,10 @@ if __name__ == '__main__':
 
         # Navigate to the desired webpage
         page.goto(HOME_PAGE)
+
+        # Get only the visible text content from the page
+        visible_text = page.evaluate('document.body.innerText')
+        result.append(visible_text)
 
         # Get all the links on the page
         anchors = page.locator('xpath=//table//a')
@@ -24,8 +32,14 @@ if __name__ == '__main__':
             links.append(HOME_PAGE + link.get_attribute("href"))
 
         # Print the list of links
-        for link in links:
-            print(link)
+        for link in tqdm(links):
+            page.goto(link)
+            # Get only the visible text content from the page
+            visible_text = page.evaluate('document.body.innerText')
+            result.append(visible_text)
 
         # Close the browser
         browser.close()
+
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
